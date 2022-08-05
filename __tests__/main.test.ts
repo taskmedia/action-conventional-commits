ecd65757c287daeb16838c5575d4896e46aee3a0
Marco Lecheler <marco@task.media>
2022-08-05 23:00:17 +0200

    chore(lint): linting typescript files

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,52 +1,52 @@
-import { expect, test } from '@jest/globals'
+import {expect, test} from '@jest/globals'
 
 import * as cc from '../src/conventionalcommit'
 
 test('commit - single', async () => {
-  let msg: string = `feat: test commit`;
+  let msg: string = `feat: test commit`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+  let commit: cc.conventionalcommit = cc.checkCommit(msg)
 
-  expect(commit.invalid).toBe(false);
-  expect(commit.full).toBe(msg);
-  expect(commit.type).toBe("feat");
-  expect(commit.breaking).toBe(false);
-  expect(commit.scope).toBe("");
-  expect(commit.message).toBe("test commit");
-  expect(commit.body).toBe("");
-  expect(commit.breaking_change).toBe("");
+  expect(commit.invalid).toBe(false)
+  expect(commit.full).toBe(msg)
+  expect(commit.type).toBe('feat')
+  expect(commit.breaking).toBe(false)
+  expect(commit.scope).toBe('')
+  expect(commit.message).toBe('test commit')
+  expect(commit.body).toBe('')
+  expect(commit.breaking_change).toBe('')
 })
 
 test('commit - scope', async () => {
-  let msg: string = `feat(cicd): test commit`;
+  let msg: string = `feat(cicd): test commit`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+  let commit: cc.conventionalcommit = cc.checkCommit(msg)
 
-  expect(commit.invalid).toBe(false);
-  expect(commit.full).toBe(msg);
-  expect(commit.type).toBe("feat");
-  expect(commit.breaking).toBe(false);
-  expect(commit.scope).toBe("cicd");
-  expect(commit.message).toBe("test commit");
-  expect(commit.body).toBe("");
-  expect(commit.breaking_change).toBe("");
+  expect(commit.invalid).toBe(false)
+  expect(commit.full).toBe(msg)
+  expect(commit.type).toBe('feat')
+  expect(commit.breaking).toBe(false)
+  expect(commit.scope).toBe('cicd')
+  expect(commit.message).toBe('test commit')
+  expect(commit.body).toBe('')
+  expect(commit.breaking_change).toBe('')
 })
 
 test('commit - message', async () => {
   let msg: string = `fix(cicd): test commit
 
-some more text`;
+some more text`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+  let commit: cc.conventionalcommit = cc.checkCommit(msg)
 
-  expect(commit.invalid).toBe(false);
-  expect(commit.full).toBe(msg);
-  expect(commit.type).toBe("fix");
-  expect(commit.breaking).toBe(false);
-  expect(commit.scope).toBe("cicd");
-  expect(commit.message).toBe("test commit");
-  expect(commit.body).toBe("some more text");
-  expect(commit.breaking_change).toBe("");
+  expect(commit.invalid).toBe(false)
+  expect(commit.full).toBe(msg)
+  expect(commit.type).toBe('fix')
+  expect(commit.breaking).toBe(false)
+  expect(commit.scope).toBe('cicd')
+  expect(commit.message).toBe('test commit')
+  expect(commit.body).toBe('some more text')
+  expect(commit.breaking_change).toBe('')
 })
 
 test('commit - breaking', async () => {
@@ -54,16 +54,16 @@ test('commit - breaking', async () => {
 
 some more text
 
-BREAKING CHANGE: API changed`;
+BREAKING CHANGE: API changed`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+  let commit: cc.conventionalcommit = cc.checkCommit(msg)
 
-  expect(commit.invalid).toBe(false);
-  expect(commit.full).toBe(msg);
-  expect(commit.type).toBe("fix");
-  expect(commit.breaking).toBe(true);
-  expect(commit.scope).toBe("cicd");
-  expect(commit.message).toBe("test commit");
-  expect(commit.body).toBe("some more text");
-  expect(commit.breaking_change).toBe("API changed");
+  expect(commit.invalid).toBe(false)
+  expect(commit.full).toBe(msg)
+  expect(commit.type).toBe('fix')
+  expect(commit.breaking).toBe(true)
+  expect(commit.scope).toBe('cicd')
+  expect(commit.message).toBe('test commit')
+  expect(commit.body).toBe('some more text')
+  expect(commit.breaking_change).toBe('API changed')
 })
--- src/main.ts
+++ src/main.ts
@@ -4,56 +4,55 @@ import * as cc from './conventionalcommit'
 
 async function run(): Promise<void> {
   try {
-    const gh_token = core.getInput('token');
-    const octokit = github.getOctokit(gh_token);
+    const gh_token = core.getInput('token')
+    const octokit = github.getOctokit(gh_token)
 
-    const { data: commit_list } = await octokit.rest.pulls.listCommits({
+    const {data: commit_list} = await octokit.rest.pulls.listCommits({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       pull_number: github.context.issue.number
-    });
+    })
 
-    let hasInvalidCommits: boolean = false;
-    let versionType: string = "patch";
-    let hasBreakingCommit: boolean = false;
-    let breaking_msg: string = "";
+    let hasInvalidCommits = false
+    let versionType = 'patch'
+    let hasBreakingCommit = false
+    let breaking_msg = ''
 
-    let commits: Array<cc.conventionalcommit> = [];
+    const commits: cc.conventionalcommit[] = []
 
     for (const c of commit_list) {
-      let commit = cc.checkCommit(c.commit.message);
+      const commit = cc.checkCommit(c.commit.message)
 
       if (commit.invalid) {
-        hasInvalidCommits = true;
-        console.log("❌ "+ commit.full);
+        hasInvalidCommits = true
+        core.info(`❌ ${commit.full}`)
       } else {
-        console.log("✅ "+ commit.getShortMessage());
+        core.info(`✅ ${commit.getShortMessage()}`)
       }
 
       if (commit.breaking) {
-        hasBreakingCommit = true;
-        versionType = "major";
-        breaking_msg = commit.breaking_change;
+        hasBreakingCommit = true
+        versionType = 'major'
+        breaking_msg = commit.breaking_change
       }
 
-      if (commit.type == "feat" && versionType != "major") {
-        versionType = "minor";
+      if (commit.type === 'feat' && versionType !== 'major') {
+        versionType = 'minor'
       }
 
-      commits.push(commit);
+      commits.push(commit)
     }
 
-    core.setOutput("breaking_commit", hasBreakingCommit);
-    core.setOutput("breaking_msg", breaking_msg);
-    core.setOutput("commits", JSON.stringify(commits));
-    core.setOutput("count_commits", commits.length);
-    core.setOutput("invalid_commits", hasInvalidCommits);
-    core.setOutput("version_type", versionType);
+    core.setOutput('breaking_commit', hasBreakingCommit)
+    core.setOutput('breaking_msg', breaking_msg)
+    core.setOutput('commits', JSON.stringify(commits))
+    core.setOutput('count_commits', commits.length)
+    core.setOutput('invalid_commits', hasInvalidCommits)
+    core.setOutput('version_type', versionType)
 
     if (hasInvalidCommits) {
-      core.setFailed("at least one commit is invalid");
+      core.setFailed('at least one commit is invalid')
     }
-
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
