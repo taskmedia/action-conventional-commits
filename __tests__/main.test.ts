7d1a1be9bbf90f95b5d11bc48d3a95e160bd014d
Marco Lecheler <marco@task.media>
2022-08-05 22:35:30 +0200

    fix(test): fix test to validate conventionalcommit.checkCommit()

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,69 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import { expect, test } from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+import * as cc from '../src/conventionalcommit'
+
+test('commit - single', async () => {
+  let msg: string = `feat: test commit`;
+
+  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+
+  expect(commit.invalid).toBe(false);
+  expect(commit.full).toBe(msg);
+  expect(commit.type).toBe("feat");
+  expect(commit.breaking).toBe(false);
+  expect(commit.scope).toBe("");
+  expect(commit.message).toBe("test commit");
+  expect(commit.body).toBe("");
+  expect(commit.breaking_change).toBe("");
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test('commit - scope', async () => {
+  let msg: string = `feat(cicd): test commit`;
+
+  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+
+  expect(commit.invalid).toBe(false);
+  expect(commit.full).toBe(msg);
+  expect(commit.type).toBe("feat");
+  expect(commit.breaking).toBe(false);
+  expect(commit.scope).toBe("cicd");
+  expect(commit.message).toBe("test commit");
+  expect(commit.body).toBe("");
+  expect(commit.breaking_change).toBe("");
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+test('commit - message', async () => {
+  let msg: string = `fix(cicd): test commit
+
+some more text`;
+
+  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+
+  expect(commit.invalid).toBe(false);
+  expect(commit.full).toBe(msg);
+  expect(commit.type).toBe("fix");
+  expect(commit.breaking).toBe(false);
+  expect(commit.scope).toBe("cicd");
+  expect(commit.message).toBe("test commit");
+  expect(commit.body).toBe("some more text");
+  expect(commit.breaking_change).toBe("");
+})
+
+test('commit - breaking', async () => {
+  let msg: string = `fix(cicd)!: test commit
+
+some more text
+
+BREAKING CHANGE: API changed`;
+
+  let commit: cc.conventionalcommit = cc.checkCommit(msg);
+
+  expect(commit.invalid).toBe(false);
+  expect(commit.full).toBe(msg);
+  expect(commit.type).toBe("fix");
+  expect(commit.breaking).toBe(true);
+  expect(commit.scope).toBe("cicd");
+  expect(commit.message).toBe("test commit");
+  expect(commit.body).toBe("some more text");
+  expect(commit.breaking_change).toBe("API changed");
 })
