62eaa395c8f86374221a1e230abb869cb39e3411
Marco Lecheler <marco@task.media>
2022-08-05 23:00:17 +0200

    chore(lint): linting typescript files

--- src/conventionalcommit.ts
+++ src/conventionalcommit.ts
@@ -1,91 +1,94 @@
 export class conventionalcommit {
-  invalid: boolean;
+  invalid: boolean
 
-  full: string;
+  full: string
 
-  type: string;
-  breaking: boolean;
-  scope: string;
-  message: string;
-  body: string;
-  breaking_change: string;
+  type: string
+  breaking: boolean
+  scope: string
+  message: string
+  body: string
+  breaking_change: string
 
   constructor() {
-    this.invalid = false;
-    this.full = "";
-    this.type = "";
-    this.breaking = false;
-    this.scope = "";
-    this.message = "";
-    this.body = "";
-    this.breaking_change = "";
+    this.invalid = false
+    this.full = ''
+    this.type = ''
+    this.breaking = false
+    this.scope = ''
+    this.message = ''
+    this.body = ''
+    this.breaking_change = ''
   }
 
   getShortMessage(): string {
-    let msg: string = "";
+    let msg = ''
 
-    msg += this.type;
-    if (this.scope != "") {
-      msg += "(" + this.scope + ")";
+    msg += this.type
+    if (this.scope !== '') {
+      msg += `(${this.scope})`
     }
     if (this.breaking) {
-      msg += "!";
+      msg += '!'
     }
-    msg += ": ";
-    msg += this.message;
+    msg += ': '
+    msg += this.message
 
-    return msg;
+    return msg
   }
 }
 
 const regex_conventionalcommit = new RegExp(
   `^(?:(?<type>(fix|feat|revert))(?:\\((?<scope>.*)\\))?(?<breaking>!?): (?<message>.*)?)` +
-  `\\n?` +
-  `(?<body>[\\S\\s]+)?$`
-);
-const regex_conventionalcommit_breaking_change = new RegExp(`(?<body>[\\s\\S]*)?BREAKING CHANGE: (?<breakingchange>[\\s\\S]*)`);
-
+    `\\n?` +
+    `(?<body>[\\S\\s]+)?$`
+)
+const regex_conventionalcommit_breaking_change = new RegExp(
+  `(?<body>[\\s\\S]*)?BREAKING CHANGE: (?<breakingchange>[\\s\\S]*)`
+)
 
 export function checkCommit(commit_msg: string): conventionalcommit {
-  let c = new conventionalcommit();
+  const c = new conventionalcommit()
 
-  c.full = commit_msg;
+  c.full = commit_msg
 
-  let result = regex_conventionalcommit.exec(commit_msg);
+  const result = regex_conventionalcommit.exec(commit_msg)
 
   if (result == null) {
-    c.invalid = true;
-    return c;
+    c.invalid = true
+    return c
   }
 
-  if (result?.groups?.type != undefined) {
-    c.type = String(result?.groups?.type).trim();
+  if (result?.groups?.type !== undefined) {
+    c.type = String(result?.groups?.type).trim()
   }
 
-  if (result?.groups?.breaking != "") {
-    c.breaking = true;
+  if (result?.groups?.breaking !== '') {
+    c.breaking = true
   }
 
-  if (result?.groups?.scope != undefined) {
-    c.scope = String(result?.groups?.scope).trim();
+  if (result?.groups?.scope !== undefined) {
+    c.scope = String(result?.groups?.scope).trim()
   }
 
-  if (result?.groups?.message != undefined) {
-    c.message = String(result?.groups?.message).trim();
+  if (result?.groups?.message !== undefined) {
+    c.message = String(result?.groups?.message).trim()
   }
 
-  if (result?.groups?.body != undefined) {
-    c.body = String(result?.groups?.body).trim();
+  if (result?.groups?.body !== undefined) {
+    c.body = String(result?.groups?.body).trim()
   }
 
-  if (c.body.includes("BREAKING CHANGE:")) {
-    c.breaking = true;
+  if (c.body.includes('BREAKING CHANGE:')) {
+    c.breaking = true
 
-    let result_breaking_change = regex_conventionalcommit_breaking_change.exec(c.body)
-    c.body = String(result_breaking_change?.groups?.body).trim();
-    c.breaking_change = String(result_breaking_change?.groups?.breakingchange).trim();
+    const result_breaking_change =
+      regex_conventionalcommit_breaking_change.exec(c.body)
+    c.body = String(result_breaking_change?.groups?.body).trim()
+    c.breaking_change = String(
+      result_breaking_change?.groups?.breakingchange
+    ).trim()
   }
 
-  return c;
-
+  return c
 }
