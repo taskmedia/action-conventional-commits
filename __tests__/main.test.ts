3ef1a08ed58fb77087f340acba4f5313a78a56f0
Marco Lecheler <marco@task.media>
2022-08-06 09:19:34 +0200

    feat: add input types
    this allows to specify different types in commit message
    
    closes #27

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,10 +2,12 @@ import {expect, test} from '@jest/globals'
 
 import * as cc from '../src/conventionalcommit'
 
+const types_default = "fix|feat|revert"
+
 test('commit - single', async () => {
   let msg: string = `feat: test commit`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg)
+  let commit: cc.conventionalcommit = cc.checkCommit(msg, types_default)
 
   expect(commit.invalid).toBe(false)
   expect(commit.full).toBe(msg)
@@ -20,7 +22,7 @@ test('commit - single', async () => {
 test('commit - scope', async () => {
   let msg: string = `feat(cicd): test commit`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg)
+  let commit: cc.conventionalcommit = cc.checkCommit(msg, types_default)
 
   expect(commit.invalid).toBe(false)
   expect(commit.full).toBe(msg)
@@ -37,7 +39,7 @@ test('commit - message', async () => {
 
 some more text`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg)
+  let commit: cc.conventionalcommit = cc.checkCommit(msg, types_default)
 
   expect(commit.invalid).toBe(false)
   expect(commit.full).toBe(msg)
@@ -56,7 +58,7 @@ some more text
 
 BREAKING CHANGE: API changed`
 
-  let commit: cc.conventionalcommit = cc.checkCommit(msg)
+  let commit: cc.conventionalcommit = cc.checkCommit(msg, types_default)
 
   expect(commit.invalid).toBe(false)
   expect(commit.full).toBe(msg)
@@ -66,4 +68,19 @@ BREAKING CHANGE: API changed`
   expect(commit.message).toBe('test commit')
   expect(commit.body).toBe('some more text')
   expect(commit.breaking_change).toBe('API changed')
+})
+
+test('commit - custom type', async () => {
+  let msg: string = `chore: test commit`
+
+  let commit: cc.conventionalcommit = cc.checkCommit(msg, "fix|feat|revert|chore")
+
+  expect(commit.invalid).toBe(false)
+  expect(commit.full).toBe(msg)
+  expect(commit.type).toBe('chore')
+  expect(commit.breaking).toBe(false)
+  expect(commit.scope).toBe('')
+  expect(commit.message).toBe('test commit')
+  expect(commit.body).toBe('')
+  expect(commit.breaking_change).toBe('')
 })
